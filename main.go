a64a459f56cc5df43765273c41ad76345a2d735d
Mostafa Negim <mostafa.negim@gmail.com>
2023-01-26 21:09:27 +0330

    add Writer type

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "text/tabwriter"
-
 func main() {
-	tabwriter.NewWriter()
+
 }
---
+++ pkg/prettytable/types.go
@@ -0,0 +1,8 @@
+package prettytable
+
+type Writer struct {
+}
+
+func (b *Writer) Write(buf []byte) (n int, err error) {
+	return 0, nil
+}
